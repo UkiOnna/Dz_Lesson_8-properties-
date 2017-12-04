141615fbc1760a461ce26a07aabb0d690a54c2ac
UkiOnna <sikirovtemirlan@mail.ru>
2017-12-04 17:32:32 +0600

    hehehey

---
+++ Dz_Properties/Dz_Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_Properties
+{
+    class Dz_Array
+    {
+        private int[] array;
+
+
+
+        public Dz_Array(int size)
+        {
+            array = new int[size];
+        }
+
+
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                array[index] = value * value;
+            }
+
+        }
+    }
+}
---
+++ Dz_Properties/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine("Введите размер массива");
+            int.TryParse(Console.ReadLine(), out size);
+            Dz_Array array = new Dz_Array(size);
+            Random random = new Random();
+            int maxSizeForRandom = 50;
+
+
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(maxSizeForRandom);
+                Console.Write("{0}  ",array[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
